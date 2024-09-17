8cab0e5b91df80e325706d96957c3b0a67d7bbe5
tinh0804 <23115053122145@sv.ute.udn.vn>
2024-09-17 13:26:51 +0700

    Mới

---
+++ Tuan2/bai2.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+public class bai2 {
+    public static void main(String[] args) {
+        char[] name={'t','i','n','h'};
+        int age=19;
+        double height=1.72;
+        boolean isLike=true;
+
+        System.out.println("Tên của tôi là:"+name);
+        System.out.println("Tuổi của tôi là:"+age);
+        System.out.println("Chiều cao của tôi là:"+height);
+        System.out.println("Có thích lập trình không:"+(isLike ? "Có" :"Không"));
+        
+    }
+}
+
