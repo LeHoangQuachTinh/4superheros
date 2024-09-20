0bcd0b8cf5f48a56ac2f84aaa082944b0870aa24
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2024-09-20 17:57:20 +0700

    add

---
+++ tuan_2/bai_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tuan_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class bai_1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("nhap vao ho va ten:");
+        String hoten =scanner.nextLine();
+        System.out.println("Hello "+hoten);
+    }
+}
