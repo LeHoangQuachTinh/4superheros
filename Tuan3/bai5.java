2659490b712993fa02f9a6202da4768cdfdbfccf
Lê Hoàng Quách Tỉnh <23115053122145@sv.ute.udn.vn>
2024-09-23 22:13:32 +0700

    New

--- Tuan3/bai5.java
+++ Tuan3/bai5.java
@@ -4,49 +4,33 @@ import java.util.Scanner;
 
 public class bai5 {
 
+   public static bool kiemtra(int a,int b,int c){
+        if((a+b>c)&&(a+c>b)&&(b+c>a))
+            return true;
+        else
+            return false;
+    }
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Nhập cạnh a: ");
-        double a = scanner.nextDouble();
-        System.out.print("Nhập cạnh b: ");
-        double b = scanner.nextDouble();
-        System.out.print("Nhập cạnh c: ");
-        double c = scanner.nextDouble();
-        
-        if (ktTamgiac(a, b, c)) {
-            System.out.println("Ba cạnh tạo thành một tam giác.");
-            
-            if (ktDeu(a, b, c)) 
-                System.out.println("Đây là tam giác đều.");
-            else if (ktCan(a, b, c))
-                System.out.println("Đây là tam giác cân.");
-            else if (ktVuong(a, b, c))
-                System.out.println("Đây là tam giác vuông.");
-            else 
-                System.out.println("Đây là tam giác thường.");
-        } 
-        else 
-            System.out.println("Ba cạnh không tạo thành một tam giác.");
-        
-    }
-
-    public static boolean ktTamgiac(double a, double b, double c) {
-        return (a+b>c) && (a+c>b) && (b+c>a);
-    }
-
-    public static boolean ktDeu(double a, double b, double c) {
-        return (a==b) && (b==c);
-    }
-
-    public static boolean ktCan(double a, double b, double c) {
-        return (a==b) || (b==c) || (a==c);
-    }
-
-    public static boolean ktVuong(double a, double b, double c) {
-        double aSquared = a * a;
-        double bSquared = b * b;
-        double cSquared = c * c;
-        return (aSquared + bSquared == cSquared) || (aSquared + cSquared == bSquared) || (bSquared + cSquared == aSquared);
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Nhập vào 3 cạnh:");
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        if(kiemtra(a,b,c))
+        {
+            if(a==b&&a==c)
+                System.out.println("Đây là tam giác đều");
+            else
+                if(a==b||a==c||b==c)
+                    System.out.println("Đây là tam giác cân");
+                else
+                    if((a*a)+(b*b)==(c*c)||(a*a)+(c*c)==(b*b)||(c*c)+(b*b)==(a*a))
+                        System.out.println("Đây là tam giác vuông");
+                    else
+                        System.out.println("Đây là tam giác thường");
+        }    
+        else
+            System.out.println("3 cạnh vừa nhập không tạo thành tam giác!");
+                        
     }
 }
