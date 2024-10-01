01f0e07b1e5e5102364f16965c0e70fb95e2b749
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2024-10-01 20:37:35 +0700

    quyen update Tuan4/bai_3.java

--- tuan_4/Bai3.java
+++ tuan_4/Bai3.java
@@ -13,16 +13,17 @@ import java.util.Scanner;
 public class Bai3 {
     public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Nhap vao mot so nguyen duong:");
-            int n = scanner.nextInt();
-            if(n<0)
-                System.out.println("Yeu cap nhap lai so nguyen duong");
-            else 
+            int n;
+            do
             {
-                int giaithua = 1;
-                for(int i=1;i<=n;i++)
-                    giaithua = giaithua *i;
-                System.out.println("Giai thua cua so " +n+ " la: " +giaithua);
+                System.out.println("nhập vào số nguyên dương:");
+                n = scanner.nextInt();
             }
+            while(n<0);
+            int giaithua = 1;
+            for(int i=1;i<=n;i++)
+            giaithua = giaithua *i;
+            System.out.println("Giai thua cua so " +n+ " la: " +giaithua);
+            
     }
 }
