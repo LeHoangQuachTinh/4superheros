b504a93a3adeaf86c4798d147cad8bcc75738e37
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2024-11-06 12:01:54 +0700

    bài tập tuần 9

---
+++ TUAN_9/TaiKhoanThanhToan.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TUAN_9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class TaiKhoanThanhToan extends TaiKhoan{
+    private String maPin;
+    private double thauChi;
+    
+    public TaiKhoanThanhToan() {
+    }
+
+    public TaiKhoanThanhToan(String maPin, double thauChi, String soTaiKhoan, String chuTaiKhoan, String matKhau) {
+        super(soTaiKhoan, chuTaiKhoan, matKhau);
+        this.maPin = maPin;
+        this.thauChi = thauChi;
+    }
+
+    @Override
+    public void nhap(Scanner scanner) {
+        super.nhap(scanner); 
+        System.out.print("Nhập mã pin:");
+        this.maPin=scanner.nextLine();  
+        System.out.print("Nhập thấu chi:");
+        this.thauChi=scanner.nextDouble();
+    }
+
+    @Override
+    public void in() {
+        System.out.println("");
+        System.out.println("Thông tin tài khoản thanh toán:"+soTaiKhoan);
+        super.in(); 
+        System.out.println("Mã pin:"+this.maPin);
+        System.out.println("Thấu chi:"+this.thauChi);
+    }   
+    
+    @Override
+    public void rutTien(double tienRut) {
+       if(tienRut<=super.soDu+this.thauChi){
+            if(tienRut<=super.soDu)
+                super.soDu-=tienRut;
+            else{
+                this.thauChi-=tienRut-super.soDu;
+                super.soDu=0;
+            }   
+            System.out.println("Rút tiền thành công:"+tienRut);
+            themGiaoDich("Rút tiền", tienRut);
+        }   
+        else
+            System.out.println("Số tiền rút không hợp lệ!");
+    }    
+
+    @Override
+    public boolean kiemTraMaPin(String maPin) {
+        return this.maPin.equals(maPin);
+    }
+    
+    @Override
+    public void chuyenKhoan(TaiKhoan tk, double tienChuyenKhoan) {
+        tk.guiTien(tienChuyenKhoan);
+        super.soDu-=tienChuyenKhoan;
+        System.out.println("Chuyển khoản thành công:"+tienChuyenKhoan);
+        themGiaoDich("Chuyển khoản", tienChuyenKhoan);
+    }
+    
+    
+}
---
+++ TUAN_9/TaiKhoanTietKiem.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TUAN_9;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class TaiKhoanTietKiem extends TaiKhoan{
+    private double laiSuat;
+    private Integer kyHan; 
+    private LocalDate ngayGui; 
+
+    public TaiKhoanTietKiem() {
+    }
+
+    public TaiKhoanTietKiem(double laiSuat, Integer kyHan, LocalDate ngayGui, String soTaiKhoan, String chuTaiKhoan, String matKhau) {
+        super(soTaiKhoan, chuTaiKhoan, matKhau);
+        this.laiSuat = laiSuat;
+        this.kyHan = kyHan;
+        this.ngayGui = ngayGui;
+    }
+
+    private boolean daDenHan() {
+        if (kyHan == null) {
+            return true; 
+        }
+        LocalDate ngayDenHan = ngayGui.plusMonths(kyHan);
+        return LocalDate.now().isAfter(ngayDenHan) || LocalDate.now().isEqual(ngayDenHan);
+    }
+
+    @Override
+    public void rutTien(double tienRut) {
+        if (!daDenHan())
+            System.out.println("Chưa đến hạn rút tiền! Bạn phải chờ đến kỳ hạn " + kyHan + " tháng.");
+        else 
+            if (this.soDu < tienRut)
+                System.out.println("Số dư không đủ để rút tiền!");
+            else
+                if(tienRut <=0)
+                    System.out.println("Tiền rút không hợp lệ!");
+                else {
+                    this.soDu -= tienRut;
+                    themGiaoDich("Rút tiền", tienRut);
+                    System.out.println("Bạn đã rút thành công: " + tienRut);
+                }
+    }
+
+    @Override
+    public double LaiXuat() {
+        if(daDenHan())
+            return super.soDu*this.laiSuat;
+        return 0;
+    }
+
+    @Override
+    public void nhap(Scanner scanner) {
+        super.nhap(scanner); 
+        System.out.print("Nhập lãi suất:");
+        laiSuat=scanner.nextDouble();
+        System.out.print("Nhập kỳ hạn(tháng):");
+        this.kyHan=scanner.nextInt();
+        scanner.nextLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.print("Nhập ngày gửi(dd/MM/yyyy):");
+        String ngayGuiStr=scanner.nextLine();
+        this.ngayGui = LocalDate.parse(ngayGuiStr, formatter);
+    }
+
+    
+    @Override
+    public void in() {
+        super.in(); 
+        System.out.println("Ngày gửi:"+this.ngayGui);
+        System.out.println("Kỳ hạn:"+this.kyHan);
+        System.out.println("Lãi xuất:"+this.laiSuat);
+        System.out.println("Tiền lãi suất là:"+LaiXuat());
+    }
+    
+}
