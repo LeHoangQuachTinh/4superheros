01555bcf8dd1fa3ac20d6415e4469b48d66320cb
Ngoc Quyen <huynhngocquyen03042005@gmail.com>
2024-11-20 10:25:58 +0700

    Xóa các tệp không cần thiết

--- NHOM/IDish.java
+++
@@ -1,9 +0,0 @@
-package NHOM;
-
-import java.util.List;
-
- public interface IDish {
-    void addDish(String tenMon, int soLuong,List<Dish> menu);
-    void editDish(String tenMon, int soLuong);
-    void deleteDish(String tenMon);
- } 
