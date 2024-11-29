2f24f4155d54dfd88c398b5e779af4bf7ccbd14c
Lê Hoàng Quách Tỉnh <23115053122145@sv.ute.udn.vn>
2024-11-29 14:29:51 +0700

    Delete BT directory

--- BT/IListAccount.java
+++
@@ -1,9 +0,0 @@
-// package BT;
-public interface IListAccount {
-    void registerAccount();
-    boolean login();
-    void editAccount();
-    void forgotPassWord();
-    void lockAccount(String matk);
-    void deleteAccount();
-} 
