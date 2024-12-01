e16989168ae3757de334199eeb0cb6d3bb6d1631
Lê Hoàng Quách Tỉnh <23115053122145@sv.ute.udn.vn>
2024-12-01 20:38:59 +0700

    Delete BT directory

--- BT/NganHang/INganHang.java
+++
@@ -1,7 +0,0 @@
-package BT.NganHang;
-interface INganHang 
-{
-    abstract void khoaChucNang(int thoiGianKhoa,String thongBao);
-    abstract void hienThi();
-    abstract double ThoiGianConLai();
-} 
